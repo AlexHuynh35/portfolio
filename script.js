109caef2cd338a0cb71bc632fff9c5eb865c4392
Alex Huynh <alexh3925@gmail.com>
2018-07-18 21:34:05 +0000

    jQuery added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 /*global $*/
+var x = 0;
 $(".headshot").click(function(){
     var colors=["white","red","orange","yellow","green","blue","purple"]
-    var x = 0;
-    if(x < 6)
+    if(x < colors.length){
+        x = x + 1;
+        $("body").css("background",colors[x])
+    }else{
+        x = 0;
+        $("body").css("background",colors[x])
+    }
+});
+$("#name").click(function(){
+    $(".headshot").attr("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1AS_8TlTZOnJyCowNHa10OvVdRuQaRU1Qi_ATzrQGJHW8hk7wcA");
 });
